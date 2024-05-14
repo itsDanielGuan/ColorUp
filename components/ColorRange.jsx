48bfd52dc232af1a14f9e847df23e7e42d9915f3
danielguan23 <danielguan23@gmail.com>
2024-05-14 16:41:42 +0800

    multivariable input and synchronisation, having issues with infinitely looping color jumping

--- components/ColorRange.jsx
+++ components/ColorRange.jsx
@@ -4,6 +4,7 @@ import { ColorTab } from './ColorTab';
 import { HexColorPicker } from "react-colorful";
 import chroma from 'chroma-js';
 import ColorGroup from './ColorGroup';
+import { stringify } from 'postcss';
 
 const findClosestNumber = (array, num) =>{
   let i = 0;
@@ -85,67 +86,152 @@ const ColorRange = () => {
   //Main Color
   const [mainColor, setMainColor] = useState("5c00ff")
     //Color Infos
-  const [hex, setHex] = useState("#FFFFFF")
-  const [currHex, setCurrHex] = useState("#FFFFFF")
-  const [HSL, setHSL] = useState([
-    360,
-    100,
-    100
-  ])
+  const [HEX, setHEX] = useState(chroma(mainColor).hex())
+  const [tempHEX, setTempHEX] = useState(chroma(mainColor).hex())
+  const [RGB, setRGB] = useState(chroma(mainColor).rgb())
+  const [tempRGB, setTempRGB] = useState(chroma(mainColor).rgb())
+  const [HSL, setHSL] = useState(chroma(mainColor).hsl())
+  const [tempHSL, setTempHSL] = useState(chroma(mainColor).hsl())
   //Range Generator
   const [colorList, setColorList] = useState([])
   useEffect(()=>{
-    const colorRange = generateColorRange(mainColor)
-    setColorList(colorRange)
-  },[mainColor])
-  
-  useEffect(()=>{
-    //if mainColor changes, propagate conversion of mainColor to all other versions
+    console.log("use effect triggered")
+    if(chroma.valid(mainColor)){
+      console.log("use effect triggered, setting main color")
+      const colorRange = generateColorRange(chroma(mainColor).hex())
+      setColorList(colorRange)
+
+      //propagate changes
+      setHEX(chroma(mainColor).hex().toUpperCase())
+      setTempHEX(chroma(mainColor).hex().toUpperCase())
+      setRGB(chroma(mainColor).rgb())
+      setTempRGB(chroma(mainColor).rgb())
+      setHSL(chroma(mainColor).hsl().map((number,index)=>{
+        if(index===0){
+          return Math.round(isNaN(number)?0:number)
+        } else {
+          return Math.round(number*100)
+        }
+      }))
+      setTempHSL(chroma(mainColor).hsl().map((number,index)=>{
+        if(index===0){
+          return Math.round(isNaN(number)?0:number)
+        } else {
+          return Math.round(number*100)
+        }
+      }))
+    }
+
+    
   },[mainColor])
 
-  const handleRandomColor = (e) => {
-    e.preventDefault();
-    setMainColor(chroma.random().hex())
-  }
-  useEffect(() => {
-    document.addEventListener("keydown", handleRandomColor);
-    return () => document.removeEventListener("keydown", handleRandomColor);
-  }, []);
 
-  const handleHexColor = (hex) => {
+
+  const handleHEXChange = (value) => {
+    console.log("Hex changed")
+    // console.log(value.length, value.length>5)
+    setTempHEX(value)
+    if(chroma.valid(value)&&value.length>5){
+      // console.log("setting Main color")
+      setMainColor(value)
+    }
     //if the hexcode is valid, setMainColor
-    //if the hexcode is valid but without #, add a # then setMainColor
     //if the hexcode is invalid, dont update. leave it be
   }
 
+  const handleRGBChange = (value, position) => {
+    console.log("RGB changed")
+    if(isNaN(value)) return false
+    setTempRGB(prev => {
+      const adjustedRGB = [...prev.slice(0, position), Number(value), ...prev.slice(position + 1)];
+      
+      if (chroma.valid(adjustedRGB, 'rgb')) {
+        setMainColor(chroma(adjustedRGB,"rgb").hex());
+      }
+      
+      return adjustedRGB;
+    });
+  };
+
+  const handleHSLChange = (value, position) => {
+    console.log("HSL changed")
+    if(isNaN(value)) return false
+    
+    setTempHSL(prev => {
+      const initialHSL = [...prev.slice(0, position), Number(value), ...prev.slice(position + 1)]
+      const adjustedHSL = [initialHSL[0],initialHSL[1]/100,initialHSL[2]/100]
+      console.log("sending to maincolor",adjustedHSL.toString())
+      if (chroma.valid(adjustedHSL, 'hsl')) {
+        console.log(chroma(adjustedHSL,"hsl").hsl(),"expected")
+        setMainColor(chroma(adjustedHSL,"hsl").hex());
+      }
+      
+      return initialHSL;
+    });
+  }
+
+  const handleBlur = (type, position) => {
+    if(type==="HEX"){
+      (tempHEX.length<6 || !chroma.valid(tempHEX))?setTempHEX(HEX):null
+    } 
+    else if(type ==="RGB"){
+      !chroma.valid(tempRGB)?setTempRGB(RGB):null
+    }
+    else if(type === "HSL"){
+      !chroma.valid(tempHSL)?setTempHSL(HSL):null
+    }
+  }
+
+  const handleRandomColor = (e) => {
+    if(e.code === 'Space') {
+      e.preventDefault()
+      setMainColor(chroma.random().hex())
+    }
+  }
+  useEffect(() => {
+    const event = new KeyboardEvent('keydown', {
+      key: 'Space',
+      code: 'Space',
+      which: 32,
+      keyCode: 32,
+    });
+    handleRandomColor(event)
+    document.addEventListener("keydown", handleRandomColor);
+    return () => document.removeEventListener("keydown", handleRandomColor);
+  }, []);
 
   return (
     <div className='container mx-auto space-y-6'>
       <div className='container mx-auto px-4 pt-16 pb-8 flex flex-col items-center gap-6'>
     	  <p className='max-w-[500px] text-center text-white font-bold text-5xl'>
-          <span style={{textDecorationColor:mainColor}} className='underline decoration-indigo-700'>Color Up</span> your websites quickly.          
+          <span style={{textDecorationColor:HEX}} className='underline decoration-indigo-700'>Color Up</span> your websites quickly.          
         </p>
         <p className='max-w-[800px] text-center text-neutral-300 text-wrap px-5'>
           Space to randomise. Use this to generate a unique color palette.
+          {chroma.valid(chroma(tempRGB).rgb(),"rgb").toString()}|{chroma(mainColor).rgb()}|{tempRGB}
         </p>
 		  </div>
       <div className='custom-layout m-auto px-5 max-w-5xl'>
-        <HexColorPicker color={mainColor} onChange={setMainColor} />
+        <HexColorPicker color={HEX} onChange={setMainColor} />
       </div>
       <div className='flex flex-col justify-normal items-center md:flex-row md:items-start md:justify-center px-5 gap-4 md:gap-12'>
-        <div className='flex flex-row items-center gap-4 px-4 py-4 border border-neutral-500 rounded-xl focus-within:'>
+        <div className='flex flex-row items-center gap-4 px-4 py-4 border border-neutral-500 rounded-xl focus-within:border-neutral-300'>
           <span className='text-neutral-500 cursor-default'>Hex</span>
-          <input className='w-20 bg-transparent text-white outline-none' type="text" maxLength="7" value={mainColor}  onChange={(value)=>{handleHexColor(value)}}/>
+          <input className='w-20 bg-transparent text-white outline-none' type="text" maxLength="7" value={tempHEX}  onChange={(e)=>{handleHEXChange(e.target.value)}} onBlur={()=>{handleBlur("HEX")}}/>
         </div>
 
-        <div className='flex flex-row items-center gap-4 px-4 py-4 border border-neutral-500 rounded-xl focus-within:'>
+        <div className='flex flex-row items-center gap-4 px-4 py-4 border border-neutral-500 rounded-xl focus-within:border-neutral-300'>
           <span className='text-neutral-500 cursor-default'>RGB</span>
-          <input className='w-20 bg-transparent text-white outline-none' type="text" maxLength="7" value={mainColor}  onChange={(value)=>{handleHexColor(value)}}/>
+          <input className='w-8 text-center bg-transparent text-white outline-none' type="text" inputmode="numeric" maxLength="3" value={tempRGB[0]}  onChange={(e)=>{handleRGBChange(e.target.value,0)}} onBlur={()=>{handleBlur("RGB", 0)}}/>
+          <input className='w-8 text-center bg-transparent text-white outline-none' type="text" inputmode="numeric" maxLength="3" value={tempRGB[1]}  onChange={(e)=>{handleRGBChange(e.target.value,1)}} onBlur={()=>{handleBlur("RGB", 1)}}/>
+          <input className='w-8 text-center bg-transparent text-white outline-none' type="text" inputmode="numeric" maxLength="3" value={tempRGB[2]}  onChange={(e)=>{handleRGBChange(e.target.value,2)}} onBlur={()=>{handleBlur("RGB", 2)}}/>
         </div>
 
-        <div className='flex flex-row items-center gap-4 px-4 py-4 border border-neutral-500 rounded-xl focus-within:'>
+        <div className='flex flex-row items-center gap-4 px-4 py-4 border border-neutral-500 rounded-xl focus-within:border-neutral-300'>
           <span className='text-neutral-500 cursor-default'>HSL</span>
-          <input className='w-20 bg-transparent text-white outline-none' type="text" maxLength="7" value={mainColor}  onChange={(value)=>{handleHexColor(value)}}/>
+          <input className='w-8 text-center bg-transparent text-white outline-none' type="text" inputmode="numeric" maxLength="3" value={tempHSL[0]}  onChange={(e)=>{handleHSLChange(e.target.value,0)}} onBlur={()=>{handleBlur("HSL", 0)}}/>
+          <input className='w-8 text-center bg-transparent text-white outline-none' type="text" inputmode="numeric" maxLength="3" value={tempHSL[1]}  onChange={(e)=>{handleHSLChange(e.target.value,1)}} onBlur={()=>{handleBlur("HSL", 1)}}/>
+          <input className='w-8 text-center bg-transparent text-white outline-none' type="text" inputmode="numeric" maxLength="3" value={tempHSL[2]}  onChange={(e)=>{handleHSLChange(e.target.value,2)}} onBlur={()=>{handleBlur("HSL", 2)}}/>
         </div>
 
       </div>
